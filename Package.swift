ee458171386aa1bf17be30c620bbaab6eedf02ec
lynnswap <65545348+lynnswap@users.noreply.github.com>
2025-12-24 21:03:41 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TabBarMenu",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TabBarMenu",
+            targets: ["TabBarMenu"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TabBarMenu"
+        ),
+        .testTarget(
+            name: "TabBarMenuTests",
+            dependencies: ["TabBarMenu"]
+        ),
+    ]
+)
